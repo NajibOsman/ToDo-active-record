df4ea3a373539909f4e07fff13a009ea5dc98fe6
NajibOsman <machidydemajka38@gmail.com>
2017-07-18 15:54:54 +0300

    Re wrote the require gems in a shortcut form in the spec_helper file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,15 @@
 ENV['RACK_ENV'] = 'test'
 
 #require the same files and gems
-require("rspec")
-require("pg")
-require("sinatra/activerecord")
-require("task")
-require("list")
+require("bundler/setup")
+Dir [File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+Bundler.require(:default, :test)
+set(:root, Dir.pwd())
+
+require('capybara/rspec')
+Capybara.app = Sinatra::Application
+set(:show exceptions, false)
+require('./app')
 
 
 #set up RSpec to clean the database between test runs
